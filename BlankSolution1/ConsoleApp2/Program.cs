3673b432cacbb168cae16bdccfec696bcf6bcf25
Heulween19 <anayang1907@gmail.com>
2022-11-15 10:32:41 +0700

    nhap ten

---
+++ BlankSolution1/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.InputEncoding = Encoding.Unicode;
+            Console.WriteLine("Nhập tên: ");
+            string name;
+            name = Console.ReadLine();
+            Console.WriteLine("tên tôi là: " + name);
+
+            Console.ReadKey();
+        }
+    }
+}
